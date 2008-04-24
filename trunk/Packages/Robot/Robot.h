ade2a30283c09c557a1e71d3d907dc7b18502435
felipe.tonello <felipe.tonello@49de399f-014b-0410-8b3a-217849077827>
2008-04-24 02:45:42 +0000

    Commit para teste

---
+++ trunk/Packages/Robot/Robot.h
@@ -0,0 +1,13 @@
+class Robot {
+    public:
+        char * getName();
+        unsigned short int getId();
+    
+    private:
+        unsigned short int Id;
+        char * Name;
+        
+        unsigned short int Degree;
+        short int posX;
+        short int posY;
+};
