cf66460053b856fbc1baed4a57f565acb84393f9
felipe.tonello <felipe.tonello@49de399f-014b-0410-8b3a-217849077827>
2008-04-24 03:17:59 +0000

    Classe esqueleto para o Robot

--- Packages/Robot/Robot.h
+++ Packages/Robot/Robot.h
@@ -1,13 +1,25 @@
-class Robot {
+#include <vector>
+
+virtual class Robot {
     public:
+        unsigned short int getRodaSize();
+        unsigned short int setRodaSize(unsigned short int cm);
         char * getName();
         unsigned short int getId();
-    
+        void setMotor(Motor motor);
+        Motor * getMotor(unsigned short int Id);
+        void setSensor(Sensor sensor);
+        Motor * getSensor(unsigned short int Id);
+
     private:
         unsigned short int Id;
+        unsigned short int RodaSize;
         char * Name;
         
-        unsigned short int Degree;
-        short int posX;
-        short int posY;
+        Vector<Motor> Motors;
+        Vector<Sensor> Sensors;
+
+        //unsigned short int Degree;
+        //short int posX;
+        //short int posY;
 };
