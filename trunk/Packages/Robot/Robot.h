33e5fbaa497b41ef771762561714ecd9328978ab
felipe.tonello <felipe.tonello@49de399f-014b-0410-8b3a-217849077827>
2008-07-12 01:29:07 +0000

    adding

--- trunk/Packages/Robot/Robot.h
+++ trunk/Packages/Robot/Robot.h
@@ -1,11 +1,14 @@
-
-//RobotQt - http://robotqt.googlecode.com/
-
-//Packages/Robot/Robot.h
-
-//Revision: $Rev$
-//Author: $Author$
-//Date: $Date$
+// 
+//  Robot.h
+//  RobotQt
+//  
+//  Created by Felipe Tonello on 2008-06-22. 
+//  Copyright 2008 Felipe Ferreri Tonello. All rights reserved.
+//  http://robotqt.googlecode.com/
+// 
+//  Revision: $Rev$
+//  Author: $Author$
+//  Date: $Date$
 
 #ifndef ROBOT_H
 #define ROBOT_H
@@ -20,29 +23,31 @@ class Position;
 class Robot
 {
 public:
-	Robot(Qstring _name, ushort _id = 1) : name( _name ), id( _id );
-	~Robot();		 
+    Robot(QString _name, ushort _id = 1) : name( _name ), id( _id );
+    ~Robot();                                           
+                                                        
+    QString getName();                                                    
+    ushort getId();                                                       
 
-	QString getName();
-	ushort getId();
+    ushort getWheelSize();
+    void setWheelSize(ushort cm);
 
-	ushort getWheelSize();
-	void setWheelSize(ushort cm);
-
-	void setMotor(Motor newMotor);
-	Motor *getMotor(ushort id);
-	void setSensor(Sensor newSensor);
-	Sensor *getSensor(ushort id);
+    void setMotor(Motor newMotor);
+    Motor *getMotor(ushort id);
+    void setSensor(Sensor newSensor);
+    Sensor *getSensor(ushort id);
+    
+    //TODO set position functions
 
 private:
-	ushort id;
-	ushort wheelSize;
-	QString name;
+    ushort id;
+    ushort wheelSize;
+    QString name;
 
-	vector<Motor> motorsVector;
-	vector<Sensor> sensorsVector;
+    vector<Motor> motorsVector;
+    vector<Sensor> sensorsVector;
 
-	Position actualPosition;
+    Position actualPosition;
 };
 
-#endif
+#endif /* ROBOT_H */
