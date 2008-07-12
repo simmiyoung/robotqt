a1597a6e2f992725462d08c8ab0c783c56e21ca0
felipe.tonello <felipe.tonello@49de399f-014b-0410-8b3a-217849077827>
2008-07-12 01:34:14 +0000

    add

---
+++ src/Packages/Robot/Robot.cpp
@@ -0,0 +1,74 @@
+// 
+//  Robot.cpp
+//  RobotQt
+//  
+//  Created by Felipe Tonello on 2008-06-22.
+//  Copyright 2008 Felipe Ferreri Tonello. All rights reserved.
+//  http://robotqt.googlecode.com/
+// 
+//  Revision: $Rev$
+//  Author: $Author$
+//  Date: $Date$
+
+#include <iostream>
+
+#include "Robot.h"
+#include "../Position/Position.h"
+
+using namespace std;
+
+Robot::Robot(string _name, ushort _id) : name( _name ), id( _id )
+{
+	cout << "Robot constructor working..." << endl;
+	setWheelSize(100); //1 meter == 100 cm
+	cout << "Robot constructor done!" << endl;
+}
+
+Robot::~Robot()
+{
+	cout << "Robot Object destructed done!" << endl;
+}
+
+QString Robot::getName()
+{
+	return name;
+}
+
+ushort Robot::getId()
+{
+	return id;
+}
+
+ushort Robot::getWheelSize()
+{
+	return wheelSize;
+}
+
+void Robot::setWheelSize(ushort cm)
+{
+	wheelSize = cm;
+}
+
+void Robot::setMotor(Motor newMotor)
+{
+	motorsVector.push_back(newMotor);
+	cout << "NewMotor added" << endl;
+}
+
+Motor Robot::getMotor(ushort id)
+{
+	//TODO implementation for the Id search
+	return motorsVector[0];
+}
+
+void Robot::setSensor(Sensor newSensor)
+{
+	sensorsVector.push_back(newSensor);
+	cout << "NewSensor added" << endl;
+}
+
+Sensor Robot::getSensor(ushort id)
+{
+	//TODO implementation for the Id search
+	return sensorsVector[0];
+}
