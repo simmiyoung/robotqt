a0528c66fd558776ee7dd6fddbbaf3fcfe769e33
felipe.tonello <felipe.tonello@49de399f-014b-0410-8b3a-217849077827>
2009-01-30 23:07:06 +0000

    Adding python robotcore sources

---
+++ trunk/pythonsrc/robotcore/core.py
@@ -0,0 +1,18 @@
+"""
+robotcore.py
+RobotQt - Robot Simulation
+
+Created by Felipe Tonello on 2009-28-01.
+Copyright 2009 Felipe Ferreri Tonello. All rights reserved.
+http://robotqt.googlecode.com/
+
+Revision: $Rev$
+Author: $Author$
+Date: $Date$
+"""
+
+#from inspect import getmembers
+#
+#class RobotCoreMC(type):
+#    def __new__(cls, name, base, d):
+#        for member, value in getmembers(name)
---
+++ trunk/pythonsrc/robotcore/motor.py
@@ -0,0 +1,15 @@
+"""
+motor.py
+RobotQt - Robot Simulation
+
+Created by Felipe Tonello on 2009-28-01.
+Copyright 2009 Felipe Ferreri Tonello. All rights reserved.
+http://robotqt.googlecode.com/
+
+Revision: $Rev$
+Author: $Author$
+Date: $Date$
+"""
+
+class Motor(object):
+    pass
